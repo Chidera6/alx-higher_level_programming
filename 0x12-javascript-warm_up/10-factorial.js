2c7e64ec35a5bb79f9747a3fec821f238651879b
Chidera6 <dictasteil@gmail.com>
2022-02-28 17:40:02 +0000

    warm up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,18 @@
 #!/usr/bin/node
-function factorial (n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+
+function factorial (a) {
+  if (a <= 0) {
+    return 0;
+  } else if (a === 1) {
+    return 1;
+  } else {
+    return (a * factorial(a - 1));
+  }
 }
 
-console.log(factorial(Number(process.argv[2])));
+const myInt = parseInt(process.argv[2]);
+if (isNaN(myInt)) {
+  console.log('1');
+} else {
+  console.log(factorial(myInt));
+}
