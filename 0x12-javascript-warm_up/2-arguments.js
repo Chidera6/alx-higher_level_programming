8479fc8ab5b5813a7ee5c20b7b0c24bad045a5be
Chidera6 <dictasteil@gmail.com>
2022-02-28 16:37:51 +0000

    warm up

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* Importing the process module */
+const process = require('process');
+
+if (argv = null) {
+console.log(process.argv);
+}
+else if(argv = 1){
+    console.log(process.argv);
+}
+else {
+console.log(process.argv);
+}
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* function to add numbers */
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
+}
